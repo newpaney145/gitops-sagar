476778cd59e03b86bbb9cecfcc1deb02d51c6ff2
sagar neupane <42377140+newpaney145@users.noreply.github.com>
2024-04-07 19:08:18 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,77 +8,18 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "sagar_dynamodb"
+    encrypt        = true
   }
 }
 
-resource "aws_s3_bucket" "tf_backend_bucket" {
-  bucket = var.tf_backend_bucket_name
-}
 
-resource "aws_s3_bucket_versioning" "tf_backend_bucket_versioning" {
-  bucket = aws_s3_bucket.tf_backend_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_object_lock_configuration" "tf_backend_bucket_object_lock" {
-  depends_on          = [aws_s3_bucket.tf_backend_bucket]
-  bucket              = aws_s3_bucket.tf_backend_bucket.id
-  object_lock_enabled = "Enabled"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_backend_bucket_sse" {
-  bucket = aws_s3_bucket.tf_backend_bucket.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+resource "aws_s3_bucket" "intern-s3-bucket" {
+  bucket = var.frontend_bucket_name
+  tags = {
+      "Name" = "sagar-bucket",
+      "Creator" = "sagarnewpaney144@gmail.com",
+      "Project" = "Intern",
+      "Deletable" = "yes"
     }
-  }
-}
 
-resource "aws_dynamodb_table" "tf_backend_bucket_state_lock" {
-  name           = "sagar_dynamodb"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  tags = {
-    "Name" = "DynamoDB Terraform State Lock Table"
-  }
-}
-resource "aws_s3_bucket" "frontend" {
-  bucket = "sagar-terraform-frontend"
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
-  tags = {
-    Name        = "FrontendApp"
-    Project     = "Intern"
-    Deletable   = "yes"
-    Creator     = "sagarnewpaney144@gmail.com"
-  }
-}
-
-resource "aws_s3_bucket_policy" "frontend_policy" {
-  bucket = aws_s3_bucket.frontend.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "s3:GetObject"
-        Effect    = "Allow"
-        Resource  = "${aws_s3_bucket.frontend.arn}/*"
-        Principal = "*"
-      },
-    ]
-  })
 }
